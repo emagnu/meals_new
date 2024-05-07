b198df0f1bfe6cf882499b7cf82390d5e949b97a
Emmanuele <emagnubcp@gmail.com>
2024-05-07 15:38:25 +0100

    155. Widgets vs Screens

--- lib/models /category.dart
+++ lib/models /category.dart
@@ -1,6 +1,17 @@
-
-
 //  //   ///
 //  Import LIBRARIES
+import 'package:flutter/material.dart';
 //  Import FILES
 //  //   ///
+
+class Category {
+  const Category({
+    required this.id,
+    required this.title,
+    this.color = Colors.orange,
+  });
+
+  final String id;
+  final String title;
+  final Color color;
+}
---
+++ lib/widgets /category_grid_item.dart
@@ -0,0 +1,14 @@
+//  //   ///
+//  Import LIBRARIES
+import 'package:flutter/material.dart';
+//  Import FILES
+//  //   ///
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
