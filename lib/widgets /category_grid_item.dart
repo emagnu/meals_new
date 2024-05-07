e9ffe5ea1225e6f07dbc9d449111476c70ea98a9
Emmanuele <emagnubcp@gmail.com>
2024-05-07 16:40:33 +0100

    157. Making any Widget Tappable with InkWell

--- lib/widgets /category_grid_item.dart
+++ lib/widgets /category_grid_item.dart
@@ -12,25 +12,31 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: <Color>[
-          category.color.withOpacity(0.55),
-          category.color.withOpacity(0.9)
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )),
-      child: Center(
-        child: Text(
-          category.title,
-          // textAlign: TextAlign.center,
-          style: Theme.of(context)
-              .textTheme
-              .titleLarge!
-              .copyWith(color: Theme.of(context).colorScheme.onSurface),
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: <Color>[
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Center(
+          child: Text(
+            category.title,
+            // textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onSurface),
+          ),
         ),
       ),
     );
